3a87598d791b1147dafd25cd044ce53f2cfaf6ad
Beata Tomczak <tma.btty@gmail.com>
2021-05-06 22:52:46 +0200

    Redux setup

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import GlobalStyle from './globalStyles'
+import { Provider } from 'react-redux'
+import store from './redux/store';
 
 ReactDOM.render(
-  <>
+  <Provider store={store}>
     <GlobalStyle />
     <App />
-  </>,
+  </Provider>,
   document.getElementById('root')
 );
 
