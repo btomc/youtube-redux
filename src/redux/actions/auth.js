e0e744795c4081801f40c2c63924803819a23417
Beata Tomczak <tma.btty@gmail.com>
2021-05-08 22:10:16 +0200

    Save user info in session storage

--- src/redux/actions/auth.js
+++ src/redux/actions/auth.js
@@ -1,41 +1,48 @@
 import firebase from 'firebase/app'
 import auth from '../../firebase'
-import { LOAD_PROFILE, LOGIN_FAIL, LOGIN_REQUEST, LOGIN_SUCCESS } from '../actionType'
+import {
+  LOAD_PROFILE,
+  LOGIN_FAIL,
+  LOGIN_REQUEST,
+  LOGIN_SUCCESS,
+} from '../actionType'
 
-export const login = () => async dispatch => {
-    try {
-        dispatch({
-            type: LOGIN_REQUEST,
-        })
+export const login = () => async (dispatch) => {
+  try {
+    dispatch({
+      type: LOGIN_REQUEST,
+    })
 
-        const provider = new firebase.auth.GoogleAuthProvider()
+    const provider = new firebase.auth.GoogleAuthProvider()
 
-        const res = await auth.signInWithPopup(provider)
-        console.log(res)
+    const res = await auth.signInWithPopup(provider)
+    // console.log(res)
 
-        const accessToken = res.credential.accessToken
+    const accessToken = res.credential.accessToken
 
-        const profile = {
-            name: res.additionalUserInfo.profile.name,
-            photoURL: res.additionalUserInfo.profile.picture,
-        }
-        console.log(profile)
-
-        dispatch({
-            type: LOGIN_SUCCESS,
-            payload: accessToken
-        })
-
-        dispatch({
-            type: LOAD_PROFILE,
-            payload: profile,
-        })
-
-    } catch (error) {
-        console.log(error.message)
-        dispatch({
-            type: LOGIN_FAIL,
-            payload: error.message,
-        })
+    const profile = {
+      name: res.additionalUserInfo.profile.name,
+      photoURL: res.additionalUserInfo.profile.picture,
     }
+    // console.log(profile)
+
+    sessionStorage.setItem('ytc-access-token', accessToken)
+    sessionStorage.setItem('ytc-user', JSON.stringify(profile))
+
+    dispatch({
+      type: LOGIN_SUCCESS,
+      payload: accessToken,
+    })
+
+    dispatch({
+      type: LOAD_PROFILE,
+      payload: profile,
+    })
+  } catch (error) {
+    console.log(error.message)
+    dispatch({
+      type: LOGIN_FAIL,
+      payload: error.message,
+    })
+  }
 }
